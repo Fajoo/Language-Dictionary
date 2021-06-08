b6f85517106224ad7571d2d6e8bf7d5e96a8871c
Dima Savchenko <dimasav57@gmail.com>
2021-06-08 13:01:59 +0300

    settings in App.xaml

--- Language Dictionary/App.xaml.cs
+++ Language Dictionary/App.xaml.cs
@@ -1,17 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
+using Forms = System.Windows.Forms;
+using Window = System.Windows.Window;
 
 namespace Language_Dictionary
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_SHOW = 5;
+
+        public static Window ActivedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+
+        public static Window FocusedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+
+        private readonly Forms.NotifyIcon _notifyIcon;
+
+        public App()
+        {
+            _notifyIcon = new Forms.NotifyIcon();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (CheckProcess()) Current.Shutdown();
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindowOnClosing;
+            MainWindow.Closed += MainWindowOnClosed;
+
+            MainWindow.Show();
+
+            _notifyIcon.Icon = new Icon("Resources/Img/lan.ico");
+            var contextMenuIcon = _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            _notifyIcon.Text = "Language Dictionary";
+
+            contextMenuIcon.Items.Add("Open", null, (obj, ev) => MainWindow.Show());
+            contextMenuIcon.Items.Add("Quit", null, (obj, ev) => App.Current.Shutdown());
+            _notifyIcon.Visible = true;
+            _notifyIcon.MouseDoubleClick += NotifyIconOnMouseDoubleClick;
+
+            base.OnStartup(e);
+        }
+
+        private bool CheckProcess()
+        {
+            var currentProcess = Process.GetCurrentProcess();
+            var runningProcess = Process.GetProcesses().FirstOrDefault(process =>
+                process.Id != currentProcess.Id &&
+                process.ProcessName.Equals(currentProcess.ProcessName, StringComparison.Ordinal));
+
+            if (runningProcess is null) return false;
+
+            ShowWindow(runningProcess.MainWindowHandle, SW_SHOW);
+            return true;
+        }
+
+        private void MainWindowOnClosed(object? sender, EventArgs e)
+        {
+            _notifyIcon.Dispose();
+        }
+
+        private void NotifyIconOnMouseDoubleClick(object sender, Forms.MouseEventArgs e)
+        {
+            MainWindow.Show();
+        }
+
+        private void MainWindowOnClosing(object sender, CancelEventArgs e)
+        {
+            MainWindow.Hide();
+            e.Cancel = true;
+        }
     }
 }
